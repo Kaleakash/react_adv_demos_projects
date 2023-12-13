0dd42b9c929322e13a63806bce1c78bbd06c6f7d
akash <akash300383@gmail.com>
2023-12-13 19:22:29 +0530

    done

--- Project with code/secion 5 - redux-thunk/airport-search-react-redux-thunk-app/src/index.js
+++ Project with code/secion 5 - redux-thunk/airport-search-react-redux-thunk-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware} from 'redux';
 import {thunk} from 'redux-thunk';
 import { Provider } from 'react-redux';
 import allReducers from '../src/services/Redux/reducers/index';
@@ -8,12 +8,9 @@ import App from './components/search/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const composeEnhancers = (window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
-  // other store enhancers if any
-);
-const store = createStore(allReducers, enhancer);
+
+
+const store = createStore(allReducers,applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
